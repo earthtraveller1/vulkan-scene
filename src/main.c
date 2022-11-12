ff1beddcef1cd316863f4726e846af4de2ef092f
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 13:11:37 -0500

    Created a basic vulkan application.

--- src/main.c
+++ src/main.c
@@ -1,16 +1,38 @@
+#include <vulkan/vulkan.h>
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv)
 {
-    puts("Hello, World!");
-    puts("Here are some of the arguments passed to this program:");
+    VkApplicationInfo app_info;
+    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+    app_info.pNext = NULL;
+    app_info.pApplicationName = "Vulkan Scene";
+    app_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    app_info.pEngineName = NULL;
+    app_info.engineVersion = 0;
+    app_info.apiVersion = VK_API_VERSION_1_2;
     
-    for (int i = 0; i < argc; i++)
+    VkInstanceCreateInfo instance_info;
+    instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instance_info.pNext = NULL;
+    instance_info.flags = 0;
+    instance_info.pApplicationInfo = &app_info;
+    instance_info.enabledLayerCount = 0;
+    instance_info.ppEnabledLayerNames = NULL;
+    instance_info.enabledExtensionCount = 0;
+    instance_info.ppEnabledExtensionNames = NULL;
+    
+    VkInstance instance;
+    
+    if (vkCreateInstance(&instance_info, NULL, &instance) != VK_SUCCESS)
     {
-        printf("\t%s\n", argv[i]);
+        puts("[FATAL ERROR]: Failed to create the instance.");
+        return EXIT_FAILURE;
     }
     
-    puts("That's all.");
+    vkDestroyInstance(instance, NULL);
     
-    return 0;
+    return EXIT_SUCCESS;
 }
