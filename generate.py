79fc11f2d2d64d469df1da4a839a36218a8378c4
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 13:04:48 -0500

    Added the directories and linked the libraries.

--- generate.py
+++ generate.py
@@ -5,16 +5,33 @@ import os
 VULKAN_INCLUDE_DIR = "deps/Vulkan-Loader/external/Vulkan-Headers/build/install/include"
 
 if os.name == "nt":
-    VULKAN_LIB = "vulkan-1"
+    VULKAN_LIB = "vulkan-1.lib"
     VULKAN_LINK_DIR = "deps/Vulkan-Loader/build/install/lib"
 
+
 def run(configuration: generator.Configuration):
-    generator.Executable("vulkan-scene", [ "src/main.c" ], configuration=configuration)
+    generator.Executable(
+        name="vulkan-scene",
+        sources=[
+            "src/main.c"
+        ],
+        configuration=configuration,
+        include_directories=[
+            VULKAN_INCLUDE_DIR
+        ],
+        link_directories=[
+            VULKAN_LINK_DIR
+        ],
+        link_libraries=[
+            VULKAN_LIB
+        ]
+    )
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "release":
         configuration = generator.Configuration.RELEASE
     else:
         configuration = generator.Configuration.DEBUG
-    
+
     run(configuration)
