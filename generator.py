f6f5f38d21da3617117796b46bf1d801a71077ba
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 11:29:35 -0500

    Rewrote the generator framework to make it more specialized.

--- generator.py
+++ generator.py
@@ -2,40 +2,57 @@ import ninja
 import os
 import enum
 
-SOURCE_ROOT = os.getcwd()
+if os.name == "nt":
+    DEFAULT_COMPILER = "cl"
+    DEFAULT_LINKER = "link"
+    
+    OBJECT_FILE_EXT = ".obj"
+    EXECUTABLE_EXT = ".exe"
+    
+    BASIC_COMPILE_OPTIONS = "-c $in -Fo:$out"
+    BASIC_LINK_OPTIONS = "$in -out:$out"
+else:
+    DEFAULT_COMPILER = "gcc"
+    DEFAULT_LINKER = "gcc"
+    
+    OBJECT_FILE_EXT = ".o"
+    EXECUTABLE_EXT = ""
 
-class TargetType(enum.Enum):
-    EXECUTABLE = 0
-    LIBRARY = 1
+    BASIC_COMPILE_OPTIONS = "-c $in -o $out"
+    BASIC_LINK_OPTIONS = "$in -o $out"
 
-class Target:
-    def __init__(self, name: str, sources: list = [], type: TargetType = TargetType.EXECUTABLE):
+class Executable:
+    def __init__(self, name: str, sources: list, compiler: str = DEFAULT_COMPILER, linker: str = DEFAULT_LINKER, compile_options: str = "", linker_options: str = "", defer_generation: bool = False):
         self.name = name
         self.sources = sources
-        self.type = type
+        self.compiler = compiler
+        self.linker = linker
+        self.compile_options = f"{compile_options} {BASIC_COMPILE_OPTIONS}"
+        self.link_options = f"{linker_options} {BASIC_LINK_OPTIONS}"
+        
+        if not defer_generation:
+            self.generate()
     
-    def generate(self, writer: ninja.Writer, compiler: str, linker: str):
-        writer.rule(name=f"{self.name}c", command=f"{compiler} -c $in -Fo:$out")
-        writer.rule(name=f"{self.name}l", command=f"{linker} $in -out:$out")
+    def generate(self):
+        output_file = open("build.ninja", "w")
+        writer = ninja.Writer(output_file, 80)
+        
+        writer.rule("cc", f"{self.compiler} {self.compile_options}")
+        writer.rule("ln", f"{self.linker} {self.link_options}")
         
         objects = []
         for source in self.sources:
-            object = f"objects/{source}.obj"
-            objects.append(object)
-            if not os.path.isdir(os.path.dirname(object)):
-                os.makedirs(os.path.dirname(object))
+            object = f"objects/{source}{OBJECT_FILE_EXT}"
+            
+            # Check if all the parent directories required to put the object f-
+            # ile in exists, and make them if they don't.
+            object_dir = os.path.dirname(object)
+            if not os.path.isdir(object_dir):
+                os.makedirs(object_dir)
+            
+            writer.build(object, "cc", source)
+            objects.append(object) # We need to store the objects so that we c-
+                                   # an build the executable later on.
         
-        for i in range(0, len(self.sources)):
-            writer.build(objects[i], f"{self.name}c", self.sources[i])
-        
-        writer.build(f"{self.name}.exe", f"{self.name}l", objects)
-
-class Project:
-    def __init__(self, targets: list = []):
-        self.targets = targets
-    
-    def generate(self, compiler: str, linker: str):
-        writer = ninja.Writer(open("build.ninja", "w"), 80)
-        
-        for target in self.targets:
-            target.generate(writer, compiler, linker)
+        writer.build(f"{self.name}{EXECUTABLE_EXT}", "ln", objects)
+        writer.close()
