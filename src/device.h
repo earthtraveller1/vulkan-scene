ca1a5a69a62bbdb77ad23dd51087229ecc1e998f
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 14:14:16 -0500

    Created the header file for the device abstraction.

---
+++ src/device.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDED_DEVICE_H
+#define INCLUDED_DEVICE_H
+
+#include <vulkan/vulkan.h>
+
+/*
+So, a basic device encapsulation system. It's meant to encapsulate all of the  
+singleton objects within this context.
+*/
+
+struct device
+{
+    VkInstance instance;
+    VkSurfaceKHR surface;
+    VkPhysicalDevice physical_device;
+    VkDevice device;
+};
+
+struct device create_new_device(const char* app_name);
+
+void destroy_device(struct device* device);
+
+#endif
