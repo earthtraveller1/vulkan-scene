a2cd3969af293c08b2c1cd4824a44f4b226b90c9
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 11:30:45 -0500

    Used the new generator framework.

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
 import generator
 
-project = generator.Project([ generator.Target("vulkan-scene", [ "src/main.c" ]) ])
-project.generate("cl", "link")
+generator.Executable("vulkan-scene", [ "src/main.c" ])
