83c027db4da3a5823ce4c462c316089a2932bf9c
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 12:08:01 -0500

    Refractored for future use.

--- generate.py
+++ generate.py
@@ -1,10 +1,13 @@
 import generator
 import sys
 
+def run(configuration: generator.Configuration):
+    generator.Executable("vulkan-scene", [ "src/main.c" ], configuration=configuration)
+
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "release":
         configuration = generator.Configuration.RELEASE
     else:
         configuration = generator.Configuration.DEBUG
     
-    generator.Executable("vulkan-scene", [ "src/main.c" ], configuration=configuration)
+    run(configuration)
