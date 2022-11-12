98eadf2a6c76c284996096793c9dd73201cb1bdf
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 12:02:41 -0500

    Made release the default.

--- generate.py
+++ generate.py
@@ -1,4 +1,10 @@
 import generator
+import sys
 
 if __name__ == "__main__":
-    generator.Executable("vulkan-scene", [ "src/main.c" ])
+    if len(sys.argv) > 1 and sys.argv[1] == "debug":
+        configuration = generator.Configuration.DEBUG
+    else:
+        configuration = generator.Configuration.RELEASE
+    
+    generator.Executable("vulkan-scene", [ "src/main.c" ], configuration=configuration)
