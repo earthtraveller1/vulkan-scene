29f02df0d57010fa268485d263591dbd35757852
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 14:21:35 -0500

    Created a basic framework for the application.

--- src/main.c
+++ src/main.c
@@ -1,38 +1,37 @@
-#include <vulkan/vulkan.h>
-
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct application
+{
+    bool is_running;
+};
+
+void initialise_application(struct application* app)
+{
+    puts("Initialising application.");
+    app->is_running = true; // Because why not?
+}
+
+void update_application(struct application* app)
+{
+    printf("Updating application %d", app->is_running);
+}
+
+void destroy_application(struct application* app)
+{
+    printf("Destroying application %d", app->is_running);
+}
 
 int main()
 {
-    VkApplicationInfo app_info;
-    app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-    app_info.pNext = NULL;
-    app_info.pApplicationName = "Vulkan Scene";
-    app_info.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
-    app_info.pEngineName = NULL;
-    app_info.engineVersion = 0;
-    app_info.apiVersion = VK_API_VERSION_1_2;
+    struct application application;
+    initialise_application(&application);
     
-    VkInstanceCreateInfo instance_info;
-    instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instance_info.pNext = NULL;
-    instance_info.flags = 0;
-    instance_info.pApplicationInfo = &app_info;
-    instance_info.enabledLayerCount = 0;
-    instance_info.ppEnabledLayerNames = NULL;
-    instance_info.enabledExtensionCount = 0;
-    instance_info.ppEnabledExtensionNames = NULL;
-    
-    VkInstance instance;
-    
-    if (vkCreateInstance(&instance_info, NULL, &instance) != VK_SUCCESS)
+    while (application.is_running)
     {
-        puts("[FATAL ERROR]: Failed to create the instance.");
-        return EXIT_FAILURE;
+        update_application(&application);
     }
     
-    vkDestroyInstance(instance, NULL);
-    
-    return EXIT_SUCCESS;
+    destroy_application(&application);
 }
