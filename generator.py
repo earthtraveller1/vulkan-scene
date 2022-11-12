4bad2a2b06b0389acabf661466ec3b2f1d2b406c
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-12 12:02:14 -0500

    Added Debug and Release configurations.

--- generator.py
+++ generator.py
@@ -11,6 +11,12 @@ if os.name == "nt":
 
     BASIC_COMPILE_OPTIONS = "-W4 -c $in -Fo:$out"
     BASIC_LINK_OPTIONS = "$in -out:$out"
+    
+    COMPILE_DEBUG_OPTIONS = "-Zi -Od"
+    COMPILE_RELEASE_OPTIONS = "-O2"
+    
+    LINK_DEBUG_OPTIONS = "-debug"
+    LINK_RELEASE_OPTIONS = ""
 else:
     DEFAULT_COMPILER = "gcc"
     DEFAULT_LINKER = "gcc"
@@ -20,14 +26,40 @@ else:
 
     BASIC_COMPILE_OPTIONS = "-Wall -pedantic -c $in -o $out"
     BASIC_LINK_OPTIONS = "$in -o $out"
+    
+    COMPILE_DEBUG_OPTIONS = "-g3 -Og"
+    COMPILE_RELEASE_OPTIONS = "-O3"
+    
+    LINK_DEBUG_OPTIONS = "-g3 -Og"
+    LINK_RELEASE_OPTIONS = "-O3"
 
+class Configuration(enum.Enum):
+    DEBUG = 0
+    RELEASE = 1
 
 class Executable:
-    def __init__(self, name: str, sources: list, compiler: str = DEFAULT_COMPILER, linker: str = DEFAULT_LINKER, compile_options: str = "", linker_options: str = "", defer_generation: bool = False):
+    def __init__(
+        self, 
+        name: str, 
+        sources: list,
+        configuration: Configuration = Configuration.DEBUG,
+        compiler: str = DEFAULT_COMPILER, 
+        linker: str = DEFAULT_LINKER, 
+        compile_options: str = "", 
+        linker_options: str = "", 
+        defer_generation: bool = False
+    ):
         self.name = name
         self.sources = sources
         self.compiler = compiler
         self.linker = linker
+        if configuration == Configuration.DEBUG:
+            compile_options = f"{compile_options} {COMPILE_DEBUG_OPTIONS}"
+            linker_options = f"{linker_options} {LINK_DEBUG_OPTIONS}"
+        elif configuration == Configuration.RELEASE:
+            compile_options = f"{compile_options} {COMPILE_RELEASE_OPTIONS}"
+            linker_options = f"{linker_options} {LINK_RELEASE_OPTIONS}"
+        
         self.compile_options = f"{compile_options} {BASIC_COMPILE_OPTIONS}"
         self.link_options = f"{linker_options} {BASIC_LINK_OPTIONS}"
 
