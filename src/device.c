7315b41253d58e9d05b47025f16e12f39e176afa
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-11-13 19:21:34 -0500

    Wrote the creation info for the debug messenger.

--- src/device.c
+++ src/device.c
@@ -3,6 +3,22 @@
 
 #include "device.h"
 
+static const VkDebugUtilsMessengerCreateInfoEXT DEBUG_MESSENGER_CREATE_INFO = {
+    /* sType = */ VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+    /* pNext = */ NULL,
+    /* flags = */ 0,
+    /* messageSeverity = */ VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+                            VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                            VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+                            VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT,
+    /* messageType = */ VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |
+                        VK_DEBUG_UTILS_MESSAGE_TYPE_DEVICE_ADDRESS_BINDING_BIT_EXT,
+    /* pfnUserCallback = */ NULL,
+    /* pUserData = */ NULL
+};
+
 static VkInstance create_instance(const char* app_name)
 {
     VkApplicationInfo application_info;
