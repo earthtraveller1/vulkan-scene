83bbd38fd3bc68ec46371ebd9949227d28e96334
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-11-13 20:18:08 -0500

    Integrated the device into the application.

--- src/main.c
+++ src/main.c
@@ -1,32 +1,59 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+
+#include "device.h"
 
 struct application
 {
     bool is_running;
+    struct device device;
 };
 
-void initialise_application(struct application* app)
+void initialise_application(struct application* app, bool enable_validation)
 {
     puts("Initialising application.");
-    app->is_running = true; // Because why not?
+    
+    create_new_device(&(app->device), "Vulkan Scene", enable_validation);
+    
+    /* There is no main loop just yet so it exits just after initialisation */
+    app->is_running = false;
 }
 
 void update_application(struct application* app)
 {
-    printf("Updating application %d\n", app->is_running);
+    puts("Updating application");
 }
 
 void destroy_application(struct application* app)
 {
-    printf("Destroying application %d\n", app->is_running);
+    puts("Destroying application");
+    
+    destroy_device(&(app->device));
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    bool enable_validation;
+    if (argc > 1)
+    {
+        if (strcmp(argv[1], "--enable-validation") == 0)
+        {
+            enable_validation = true;
+        }
+        else 
+        {
+            enable_validation = false;
+        }
+    }
+    else 
+    {
+        enable_validation = false;
+    }
+    
     struct application application;
-    initialise_application(&application);
+    initialise_application(&application, enable_validation);
     
     while (application.is_running)
     {
