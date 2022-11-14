228a4ecae6959db20be2321f49157f1f11c0c173
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-14 17:55:01 -0500

    Removed code for cloning repositories.

--- build_deps.py
+++ build_deps.py
@@ -3,15 +3,6 @@ import sys
 import shutil
 
 def run(configuration: str):
-    if not os.path.isdir("deps"):
-        os.makedirs("deps")
-    
-    if os.path.isdir("deps/Vulkan-Loader"):
-        print("[ERROR]: deps/Vulkan-Loader already exists. Please remove it or do something.")
-        return
-    
-    os.system("git clone --depth=1 https://github.com/KhronosGroup/Vulkan-Loader.git deps/Vulkan-Loader")
-    
     os.system(f"cmake -S deps/Vulkan-Loader -B deps/Vulkan-Loader/build -D UPDATE_DEPS=true -D CMAKE_BUILD_TYPE={configuration} -G Ninja")
     os.system(f"cmake --build deps/Vulkan-Loader/build")
     os.system(f"cmake --install deps/Vulkan-Loader/build --prefix deps/Vulkan-Loader/build/install --config {configuration}")
