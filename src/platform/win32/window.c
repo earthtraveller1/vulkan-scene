4fac9ea3783fca9e7fb4d2bcbb2e7afa21d8a084
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-16 17:57:07 -0500

    Created the Window procedure.

--- src/platform/win32/window.c
+++ src/platform/win32/window.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "../../window.h"
 
@@ -10,20 +11,45 @@
 struct window
 {
     HINSTANCE h_instance; /* May be required for some things, but not completely sure. */
+    bool is_open;
     HWND window;
 };
 
 const LPCTSTR WINDOW_CLASS_NAME = L"Vulkan Scene Window Class";
+
+static LRESULT CALLBACK window_procedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    struct window* window;
+    if (uMsg == WM_CREATE)
+    {
+        window = (struct window*)(((CREATESTRUCTW*)lParam)->lpCreateParams);
+        SetWindowLongPtrW(hwnd, GWLP_USERDATA, (LONG_PTR)window);
+    }
+    else
+    {
+        window = (struct window*)GetWindowLongPtrW(hwnd, GWLP_USERDATA);
+    }
+    
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            window->is_open = false;
+            return 0;
+        default:
+            return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+    }
+}
 
 struct window* create_window(uint16_t width, uint16_t height, const char* title)
 {
     struct window* window = malloc(sizeof(struct window));
     
     window->h_instance = GetModuleHandleW(NULL);
+    window->is_open = true;
     
     WNDCLASSW window_class;
     window_class.style = CS_OWNDC;
-    window_class.lpfnWndProc = DefWindowProcW;
+    window_class.lpfnWndProc = window_procedure;
     window_class.cbClsExtra = 0;
     window_class.cbWndExtra = 0;
     window_class.hInstance = window->h_instance;
@@ -35,7 +61,7 @@ struct window* create_window(uint16_t width, uint16_t height, const char* title)
     
     RegisterClassW(&window_class);
     
-    window->window = CreateWindowExW(WS_OVERLAPPEDWINDOW, WINDOW_CLASS_NAME, title, 0, 0, 0, width, height, NULL, NULL, window->h_instance, NULL);
+    window->window = CreateWindowExW(WS_OVERLAPPEDWINDOW, WINDOW_CLASS_NAME, title, 0, 0, 0, width, height, NULL, NULL, window->h_instance, window);
     
     if (window->window == NULL)
     {
