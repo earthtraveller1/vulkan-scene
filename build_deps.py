792281248cdd4f60aff76b14743002faa296deb4
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-16 17:39:14 -0500

    Fixed some things with the architecture of the dependencies.

--- build_deps.py
+++ build_deps.py
@@ -3,9 +3,17 @@ import sys
 import shutil
 
 def run(configuration: str):
-    os.system(f"cmake -S deps/Vulkan-Loader -B deps/Vulkan-Loader/build -D UPDATE_DEPS=true -D CMAKE_BUILD_TYPE={configuration} -G Ninja")
-    os.system(f"cmake --build deps/Vulkan-Loader/build")
-    os.system(f"cmake --install deps/Vulkan-Loader/build --prefix deps/Vulkan-Loader/build/install --config {configuration}")
+    configure_command = f"cmake -S deps/Vulkan-Loader -B deps/Vulkan-Loader/build -D UPDATE_DEPS=true"
+    
+    if os.name != "nt":
+        configure_command += "-D CMAKE_BUILD_TYPE={configuration} -G Ninja"
+    
+    build_command = f"cmake --build deps/Vulkan-Loader/build"
+    install_command = f"cmake --install deps/Vulkan-Loader/build --prefix deps/Vulkan-Loader/build/install --config {configuration}"
+    
+    os.system(configure_command)
+    os.system(build_command)
+    os.system(install_command)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
