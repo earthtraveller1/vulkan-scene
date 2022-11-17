2dec06238e7e4b7effe7e425b024fcd9347b1638
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-16 19:23:46 -0500

    Added a way to mark stuff as unused.

---
+++ src/utils.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDED_UTILS_H
+#define INCLUDED_UTILS_H
+
+/* A set of common utilities that might be used that doesn't fall into any par-
+ticular category. */
+
+#define UNUSED(x) (void)(x)
+
+#endif
