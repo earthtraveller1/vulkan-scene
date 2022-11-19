6b981b378206f7914a2cc4bfb87a4a903a13945b
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-19 16:54:07 -0500

    Removed magic number.

--- src/device.c
+++ src/device.c
@@ -35,13 +35,13 @@ static const VkDebugUtilsMessengerCreateInfoEXT DEBUG_MESSENGER_CREATE_INFO = {
     /* pUserData = */ NULL
 };
 
-/* Platform independent extensions. */
-static const char* REQUIRED_DEVICE_EXTENSIONS[1] = {
-    VK_KHR_SWAPCHAIN_EXTENSION_NAME
-};
-
 /* Number of required device extensions. */
 #define REQUIRED_DEVICE_EXTENSION_COUNT 1
+
+/* Platform independent extensions. */
+static const char* REQUIRED_DEVICE_EXTENSIONS[REQUIRED_DEVICE_EXTENSION_COUNT] = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debug_messenger_callback(
     VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
