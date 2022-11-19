f93d290d07ba3c423c74eed4675167a32a689297
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-19 17:57:19 -0500

    Ran the commands instead of printing them out.

--- tools/cmake.py
+++ tools/cmake.py
@@ -11,11 +11,11 @@ def configure(source_dir: str, binary_dir: str, variables: dict | None = None, g
         for variable in variables.keys():
             command += f" -D{variable}={variables[variable]}"
     
-    print(command) # Just printing out to see if it works.
+    os.system(command) # Just printing out to see if it works.
 
 def build(binary_dir: str):
     command = f"cmake --build {binary_dir}"
-    print(command)
+    os.system(command)
 
 def install(build_dir: str, install_dir: str | None = None, config: str | None = None):
     command = f"cmake --install {build_dir}"
@@ -23,4 +23,4 @@ def install(build_dir: str, install_dir: str | None = None, config: str | None =
         command += f" --prefix {install_dir}"
     if config != None:
         command += f" --config {config}"
-    print(command)
+    os.system(command)
