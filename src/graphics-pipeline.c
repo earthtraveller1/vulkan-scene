416f152dfd2767f27fbc699034b18de11562c234
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-02 20:08:17 -0500

    Created a function to load and create a shader module.

--- src/graphics-pipeline.c
+++ src/graphics-pipeline.c
@@ -1,6 +1,47 @@
 #include <vulkan/vulkan.h>
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 #include "graphics-pipeline.h"
+
+static bool load_shader_module(const char* filename, VkDevice device, VkShaderModule* shader_module)
+{
+    FILE* file = fopen(filename, "rb");
+    if (!file)
+    {
+        fprintf(stderr, "[ERROR]: Failed to open %s.\n", filename);
+        return false;
+    }
+    
+    fseek(file, 0, SEEK_END);
+    size_t file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    uint32_t* file_contents = malloc(file_size * 4);
+    
+    for (uint32_t* h = file_contents; h < file_contents + file_size; h++)
+    {
+        *h = getc(file);
+    }
+    
+    VkShaderModuleCreateInfo create_info;
+    create_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    create_info.pNext = NULL;
+    create_info.flags = 0;
+    create_info.codeSize = file_size * 4;
+    create_info.pCode = file_contents;
+    
+    VkResult result = vkCreateShaderModule(device, &create_info, NULL, shader_module);
+    if (result != VK_SUCCESS)
+    {
+        fprintf(stderr, "[ERROR]: Failed to create a shader module. Vulkan error %d.\n", result);
+        return false;
+    }
+    
+    return true;
+}
 
 void create_new_graphics_pipeline(struct graphics_pipeline* pipeline, struct device* device)
 {
