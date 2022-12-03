080263acee085f09ce30fba527e10c1b7a339bb6
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-02 19:12:55 -0500

    Created skeleton implementations for the graphics pipeline class.

---
+++ src/graphics-pipeline.c
@@ -0,0 +1,13 @@
+#include <vulkan/vulkan.h>
+
+#include "graphics-pipeline.h"
+
+void create_new_graphics_pipeline(struct graphics_pipeline* pipeline, struct device* device)
+{
+    
+}
+
+void destroy_graphics_pipeline(struct graphics_pipeline* pipeline)
+{
+    
+}
