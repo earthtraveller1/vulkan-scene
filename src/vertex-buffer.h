db2edc990903b3f772813ec86846a1c558fefabe
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-05 18:56:25 -0500

    Added a bit of documentation.

--- src/vertex-buffer.h
+++ src/vertex-buffer.h
@@ -25,6 +25,24 @@ struct vertex_buffer
     VkBuffer buffer;
 };
 
+/**
+\brief Creates a vertex buffer.
+
+Creates and allocates a vertex buffer from the device that was passed as a
+parameter, and then copies the `data` into the buffer on the GPU. Please note
+that if `data_len` is greater than the actual length of the data, it will
+result in undefined behaviour!
+
+When the function fails, the error message will be printed to stderr, and a 
+return value of `false` will indicate failure to the caller.
+
+\param buffer The vertex buffer that the resulting object will be written to.
+\param device The device that the vertex buffer will be created from.
+\param data Pointer to the data that will be copied into the vertex buffer.
+\param data_len Length of the data, in number of 
+
+\returns `true` if the function succeeds, `false` if the function fails.
+*/
 bool create_vertex_buffer(struct vertex_buffer* buffer, struct device* device, const struct vertex* data, size_t data_len);
 
 #endif
