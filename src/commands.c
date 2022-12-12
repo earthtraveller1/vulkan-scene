7897db5bd73b7c2caee7269a8d48d7d8041f7442
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-12 17:57:26 -0500

    Implemented command buffer allocation.

--- src/commands.c
+++ src/commands.c
@@ -21,3 +21,22 @@ bool create_new_command_pool(const struct device* device, struct command_pool* s
     
     return true;
 }
+
+bool create_new_command_buffer(const struct command_pool* self, VkCommandBuffer* command_buffer)
+{
+    VkCommandBufferAllocateInfo alloc_info;
+    alloc_info.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    alloc_info.pNext = NULL;
+    alloc_info.commandPool = self->command_pool;
+    alloc_info.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    alloc_info.commandBufferCount = 1;
+    
+    VkResult result = vkAllocateCommandBuffers(self->device->device, &alloc_info, command_buffer);
+    if (result != VK_SUCCESS)
+    {
+        fprintf(stderr, "[ERROR]: Failed to allocate command buffer. Vulkan error %d.\n", result);
+        return false;
+    }
+    
+    return true;
+}
