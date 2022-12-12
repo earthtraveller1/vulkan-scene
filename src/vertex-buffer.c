88a915a148ef5dad55bf600ed2f54c6205de0224
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-12 17:21:07 -0500

    Fixed compiler errors and warnings.

--- src/vertex-buffer.c
+++ src/vertex-buffer.c
@@ -1,15 +1,16 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "vertex-buffer.h"
 
-extern const VkVertexInputBindingDescription VERTEX_BINDING_DESCRIPTION = {
+const VkVertexInputBindingDescription VERTEX_BINDING_DESCRIPTION = {
     /* binding = */ 0,
     /* stride = */ sizeof(struct vertex),
     /* inputRate = */ VK_VERTEX_INPUT_RATE_VERTEX
 };
 
-extern const VkVertexInputAttributeDescription VERTEX_ATTRIBUTE_DESCRIPTIONS[VERTEX_ATTRIBUTE_DESCRIPTION_COUNT] = {
+const VkVertexInputAttributeDescription VERTEX_ATTRIBUTE_DESCRIPTIONS[VERTEX_ATTRIBUTE_DESCRIPTION_COUNT] = {
     {
         /* location = */ 0,
         /* binding = */ 0,
@@ -33,9 +34,10 @@ static uint32_t get_memory_type(uint32_t type_filter, VkMemoryPropertyFlags prop
     }
     
     *found = false;
+    return 0;
 }
 
-bool create_and_fill_staging_buffer(VkDevice device, VkPhysicalDevice physical_device, const struct vertex* data, size_t data_len, VkBuffer* buffer, VkDeviceMemory* memory)
+static bool create_and_fill_staging_buffer(VkDevice device, VkPhysicalDevice physical_device, const struct vertex* data, size_t data_len, VkBuffer* buffer, VkDeviceMemory* memory)
 {
     VkBufferCreateInfo create_info;
     create_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
@@ -82,9 +84,9 @@ bool create_and_fill_staging_buffer(VkDevice device, VkPhysicalDevice physical_d
     
     vkBindBufferMemory(device, staging_buffer, staging_buffer_memory, 0);
     
-    void* data;
-    vkMapMemory(device, staging_buffer_memory, 0, data_len * sizeof(struct vertex), 0, &data);
-    memcpy(data, data, data_len * sizeof(struct vertex));
+    void* staging_buffer_ptr;
+    vkMapMemory(device, staging_buffer_memory, 0, data_len * sizeof(struct vertex), 0, &staging_buffer_ptr);
+    memcpy(staging_buffer_ptr, data, data_len * sizeof(struct vertex));
     vkUnmapMemory(device, staging_buffer_memory);
     
     /* Only if the buffer is filled do we return it to the caller. */
@@ -138,6 +140,8 @@ bool create_vertex_buffer(struct vertex_buffer* self, struct device* device, con
         fprintf(stderr, "[ERROR]: Failed to allocate memory. Vulkan error %d.\n", result);
         return false;
     }
+    
+    return true;
 }
 
 void destroy_vertex_buffer(struct vertex_buffer* self)
