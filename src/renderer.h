90f81adacf4487bcfece9f2b6964b829366d9048
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-20 18:13:23 -0500

    Added a rendering data struct to hold all the rendering parameters.

--- src/renderer.h
+++ src/renderer.h
@@ -7,8 +7,22 @@
  * \file A basic abstraction for rendering with the Vulkan API.
 */
 
+struct rendering_data
+{
+    const struct device* device;
+    const struct swap_chain* swap_chain;
+    const struct graphics_pipeline* pipeline;
+    const struct framebuffer_manager* framebuffers;
+    const struct vertex_buffer* vertex_buffer;
+    
+    VkSemaphore image_available_semaphore;
+    VkSemaphore render_finished_semaphore;
+    
+    VkCommandBuffer command_buffer;
+};
+
 /* A basic placeholder function. The actual rendering framework will be more
 advanced than this later on. */
-void draw();
+bool draw(const struct rendering_data* data);
 
 #endif
