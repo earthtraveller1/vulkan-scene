890f675fb1ed5a15612a2ac6f62e588741d4f121
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-20 17:47:25 -0500

    Replaced it all with a draw function.

--- src/renderer.h
+++ src/renderer.h
@@ -7,18 +7,8 @@
  * \file A basic abstraction for rendering with the Vulkan API.
 */
 
-/**
- * \brief Start rendering.
-*/
-void begin_renderer(VkCommandBuffer cmd_buffer);
-
-/* A placeholder function. Right now, I'm just trying to get triangle working
-on the screen. */
+/* A basic placeholder function. The actual rendering framework will be more
+advanced than this later on. */
 void draw();
-
-/**
- * \brief Stop rendering.
-*/
-void end_renderer(VkCommandBuffer cmd_buffer);
 
 #endif
