bad20edceb6128ac6a491bc79509c2485520d5b8
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-12-20 17:26:27 -0500

    Made the order in the documentation comments match the order of the actual parameters.

--- src/synchronization.h
+++ src/synchronization.h
@@ -12,9 +12,9 @@
  * \brief Creates a Vulkan semaphore. Needs to be destroyed with 
  * `vkDestroySemaphore`.
  * 
+ * \param device The device object you would like to create this from.
  * \param semaphore The address that the created semaphore handle should be
  * stored at.
- * \param device The device object you would like to create this from.
  * 
  * \returns A boolean that indicates whether the procedure has succeeded.
 */
@@ -24,9 +24,9 @@ bool create_vulkan_semaphore(const struct device* device, VkSemaphore* semaphore
  * \brief Creates a Vulkan fence. Needs to be destroyed with 
  * `vkDestroyFence`.
  * 
+ * \param device The device object that you would like to create it from.
  * \param fence The address that the created fence handle should be
  * stored at.
- * \param device The device object that you would like to create it from.
  * 
  * \returns Whether the procedure has failed or not.
 */
