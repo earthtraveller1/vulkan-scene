7166e04a0cca52c1f73fec7f1c101eb605bd86e0
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-23 09:58:10 -0500

    Integrated the renderer into the application.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,8 @@
 #include "window.h"
 #include "vertex-buffer.h"
 #include "framebuffer-manager.h"
+#include "renderer.h"
+#include "synchronization.h"
 
 #define WWIDTH 800
 #define WHEIGHT 600
@@ -23,6 +25,8 @@ struct application
     struct swap_chain swap_chain;
     struct graphics_pipeline graphics_pipeline;
     struct framebuffer_manager framebuffer_manager;
+    
+    struct rendering_data rendering_data;
     
     struct vertex_buffer vertex_buffer;
 };
@@ -70,6 +74,18 @@ bool initialise_application(struct application* app, bool enable_validation)
         fputs("[ERROR]: Failed to create the framebuffer manager.\n", stderr);
         return false;
     }
+    
+    app->rendering_data.device = &app->device;
+    app->rendering_data.swap_chain = &app->swap_chain;
+    app->rendering_data.pipeline = &app->graphics_pipeline;
+    app->rendering_data.framebuffers = &app->framebuffer_manager;
+    app->rendering_data.vertex_buffer = &app->vertex_buffer;
+    
+    create_vulkan_semaphore(&app->device, &app->rendering_data.image_available_semaphore);
+    create_vulkan_semaphore(&app->device, &app->rendering_data.render_finished_semaphore);
+    create_vulkan_fence(&app->device, &app->rendering_data.in_flight_fence);
+    
+    create_new_command_buffer(get_command_pool_from_device(&app->device), &app->rendering_data.command_buffer);
 
     app->is_running = true;
 
@@ -78,6 +94,8 @@ bool initialise_application(struct application* app, bool enable_validation)
 
 void update_application(struct application* app)
 {
+    draw(&app->rendering_data);
+    
     update_window(app->window);
 
     app->is_running = is_window_open(app->window);
@@ -86,6 +104,12 @@ void update_application(struct application* app)
 void destroy_application(struct application* app)
 {
     puts("Destroying application");
+    
+    device_wait_idle(&app->device);
+    
+    vkDestroyFence(app->device.device, app->rendering_data.in_flight_fence, NULL);
+    vkDestroySemaphore(app->device.device, app->rendering_data.render_finished_semaphore, NULL);
+    vkDestroySemaphore(app->device.device, app->rendering_data.image_available_semaphore, NULL);
     
     destroy_framebuffer_manager(&app->framebuffer_manager);
     destroy_vertex_buffer(&app->vertex_buffer);
