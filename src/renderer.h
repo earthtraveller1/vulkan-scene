a5d368a2b1a05ec3fb7de20b07689e33b50d8707
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-24 12:17:57 -0500

    Removed commented out code.

--- src/renderer.h
+++ src/renderer.h
@@ -13,25 +13,6 @@
  * \file A basic abstraction for rendering with the Vulkan API.
  */
 
-/* struct rendering_data
-{
-    const struct device* device;
-    const struct swap_chain* swap_chain;
-    const struct graphics_pipeline* pipeline;
-    const struct framebuffer_manager* framebuffers;
-    const struct vertex_buffer* vertex_buffer;
-
-    VkSemaphore image_available_semaphore;
-    VkSemaphore render_finished_semaphore;
-    VkFence in_flight_fence;
-
-    VkCommandBuffer command_buffer;
-};
-
-/* A basic placeholder function. The actual rendering framework will be more
-advanced than this later on. /
-bool draw(const struct rendering_data* data); */
-
 struct renderer
 {
     struct device device;
