e29d0efabd7d884ec9b88a0698a08883c47d6327
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-12-26 17:28:50 -0500

    Used the utility function to create buffers.

--- src/vertex-buffer.c
+++ src/vertex-buffer.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "commands.h"
+#include "utils.h"
 
 #include "vertex-buffer.h"
 
@@ -40,86 +41,31 @@ static uint32_t get_memory_type(uint32_t type_filter,
     return 0;
 }
 
-static bool create_and_fill_staging_buffer(VkDevice device,
-                                           VkPhysicalDevice physical_device,
+static bool create_and_fill_staging_buffer(const struct device* device,
                                            const struct vertex* data,
                                            size_t data_len, VkBuffer* buffer,
                                            VkDeviceMemory* memory)
 {
-    VkBufferCreateInfo create_info;
-    create_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    create_info.pNext = NULL;
-    create_info.flags = 0;
-    create_info.size = data_len * sizeof(struct vertex);
-    create_info.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
-    create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-    create_info.queueFamilyIndexCount = 0;
-    create_info.pQueueFamilyIndices = NULL;
-
-    VkBuffer staging_buffer;
-    VkResult result =
-        vkCreateBuffer(device, &create_info, NULL, &staging_buffer);
-    if (result != VK_SUCCESS)
+    if (!create_vulkan_buffer(data_len * sizeof(struct vertex),
+                              VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                              VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                                  VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                              device, buffer, memory))
     {
-        fprintf(
-            stderr,
-            "[ERROR]: Failed to create the staging buffer. Vulkan error %d.\n",
-            result);
+        fputs("[ERROR]: Failed to create the staging buffer.\n", stderr);
         return false;
     }
 
-    VkMemoryRequirements memory_requirements;
-    vkGetBufferMemoryRequirements(device, staging_buffer, &memory_requirements);
-
-    bool found_memory_type = false;
-    uint32_t memory_type =
-        get_memory_type(memory_requirements.memoryTypeBits,
-                        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-                        physical_device, &found_memory_type);
-    if (!found_memory_type)
-    {
-        fprintf(stderr,
-                "[ERROR]: Failed to find the memory type for the staging "
-                "buffer. Vulkan error %d.\n",
-                result);
-        return false;
-    }
-
-    VkMemoryAllocateInfo allocate_info;
-    allocate_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    allocate_info.pNext = NULL;
-    allocate_info.allocationSize = data_len * sizeof(struct vertex);
-    allocate_info.memoryTypeIndex = memory_type;
-
-    VkDeviceMemory staging_buffer_memory;
-    result =
-        vkAllocateMemory(device, &allocate_info, NULL, &staging_buffer_memory);
-    if (result != VK_SUCCESS)
-    {
-        fprintf(stderr,
-                "[ERROR]: Failed to allocate memory for the staging buffer. "
-                "Vulkan error %d.\n",
-                result);
-        return false;
-    }
-
-    vkBindBufferMemory(device, staging_buffer, staging_buffer_memory, 0);
-
-    void* staging_buffer_ptr;
-    vkMapMemory(device, staging_buffer_memory, 0,
-                data_len * sizeof(struct vertex), 0, &staging_buffer_ptr);
+    void* buffer_ptr;
+    vkMapMemory(device->device, *memory, 0, data_len * sizeof(struct vertex), 0,
+                &buffer_ptr);
 #ifdef _MSC_VER
     memcpy_s(staging_buffer_ptr, data_len * sizeof(struct vertex), data,
              data_len * sizeof(struct vertex));
 #else
-    memcpy(staging_buffer_ptr, data, data_len * sizeof(struct vertex));
+    memcpy(buffer_ptr, data, data_len * sizeof(struct vertex));
 #endif
-    vkUnmapMemory(device, staging_buffer_memory);
-
-    /* Only if the buffer is filled do we return it to the caller. */
-    *buffer = staging_buffer;
-    *memory = staging_buffer_memory;
+    vkUnmapMemory(device->device, *memory);
 
     return true;
 }
@@ -182,66 +128,20 @@ bool create_vertex_buffer(struct vertex_buffer* self,
 
     const VkDeviceSize buffer_size = data_len * sizeof(struct vertex);
 
-    VkBufferCreateInfo create_info;
-    create_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    create_info.pNext = NULL;
-    create_info.flags = 0;
-    create_info.size = buffer_size;
-    create_info.usage =
-        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
-    create_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-    create_info.queueFamilyIndexCount = 0;
-    create_info.pQueueFamilyIndices = NULL;
-
-    VkResult result =
-        vkCreateBuffer(device->device, &create_info, NULL, &self->buffer);
-    if (result != VK_SUCCESS)
+    if (!create_vulkan_buffer(buffer_size,
+                              VK_BUFFER_USAGE_VERTEX_BUFFER_BIT |
+                                  VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+                              VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, device,
+                              &self->buffer, &self->memory))
     {
-        fprintf(stderr,
-                "[ERROR]: Failed to create a vertex buffer. Vulkan error %d.\n",
-                result);
+        fputs("[ERROR]: Failed to create a Vulkan buffer.\n", stderr);
         return false;
     }
-
-    VkMemoryRequirements memory_requirements;
-    vkGetBufferMemoryRequirements(device->device, self->buffer,
-                                  &memory_requirements);
-
-    bool found_memory_type;
-    uint32_t memory_type = get_memory_type(
-        memory_requirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-        device->physical_device, &found_memory_type);
-    if (!found_memory_type)
-    {
-        fputs("[ERROR]: Was unable to locate an adequate memory type for the "
-              "buffer.\n",
-              stderr);
-        return false;
-    }
-
-    VkMemoryAllocateInfo allocate_info;
-    allocate_info.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-    allocate_info.pNext = NULL;
-    allocate_info.allocationSize = memory_requirements.size;
-    allocate_info.memoryTypeIndex = memory_type;
-
-    result =
-        vkAllocateMemory(device->device, &allocate_info, NULL, &self->memory);
-    if (result != VK_SUCCESS)
-    {
-        fprintf(stderr,
-                "[ERROR]: Failed to allocate memory. Vulkan error %d.\n",
-                result);
-        return false;
-    }
-
-    vkBindBufferMemory(device->device, self->buffer, self->memory, 0);
 
     VkBuffer staging_buffer;
     VkDeviceMemory staging_buffer_memory;
-    if (!create_and_fill_staging_buffer(
-            self->device->device, self->device->physical_device, data, data_len,
-            &staging_buffer, &staging_buffer_memory))
+    if (!create_and_fill_staging_buffer(device, data, data_len, &staging_buffer,
+                                        &staging_buffer_memory))
     {
         fputs("[ERROR]: Failed to create a vertex buffer.\n", stderr);
         return false;
