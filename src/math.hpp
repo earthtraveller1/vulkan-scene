d61a7caa737c4a9c41f0825b37dcd2a5c12be763
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-01-21 14:12:48 -0500

    Created basic vector types.

---
+++ src/math.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstddef>
+
+// A basic linear algebra library. Contains no more than what this project requires.
+
+namespace vulkan_scene
+{
+
+// Represents a Vector of any size.
+template<typename T, std::size_t size>
+struct Vector
+{
+    T v[size];
+};
+
+// Different types of vectors.
+using Vector2 = Vector<float, 2>;
+using Vector3 = Vector<float, 3>;
+using Vector4 = Vector<float, 4>;
+
+}
