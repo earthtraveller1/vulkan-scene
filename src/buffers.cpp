d6e11f80f0ac4806a38cb3834e6c4aeb48bb10a7
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-01-23 20:21:46 -0500

    Added utility function for copying buffers.

--- src/buffers.cpp
+++ src/buffers.cpp
@@ -1,8 +1,10 @@
 #include "device.hpp"
+#include "utils.hpp"
 
 #include "buffers.hpp"
 
 using namespace std::string_literals;
+using vulkan_scene::Device;
 
 namespace
 {
@@ -34,7 +36,7 @@ std::uint32_t find_memory_type_index(VkPhysicalDevice p_device,
     throw std::runtime_error("Failed to find the memory type index.");
 }
 
-template<BufferType buffer_type>
+template <BufferType buffer_type>
 std::tuple<VkBuffer, VkDeviceMemory>
 create_buffer(const vulkan_scene::Device& p_device, VkDeviceSize p_size)
 {
@@ -76,9 +78,10 @@ create_buffer(const vulkan_scene::Device& p_device, VkDeviceSize p_size)
                                   &memory_requirements);
 
     const auto memory_properties =
-        buffer_type == BufferType::Staging ? VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
-                                            VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
-                                      : VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+        buffer_type == BufferType::Staging
+            ? VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                  VK_MEMORY_PROPERTY_HOST_COHERENT_BIT
+            : VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
     std::uint32_t memory_type_index = find_memory_type_index(
         p_device.get_raw_physical_handle(), memory_requirements.memoryTypeBits,
         memory_properties);
@@ -92,7 +95,7 @@ create_buffer(const vulkan_scene::Device& p_device, VkDeviceSize p_size)
 
     VkDeviceMemory memory;
     const auto result2 = vkAllocateMemory(p_device.get_raw_handle(),
-                                         &allocate_info, nullptr, &memory);
+                                          &allocate_info, nullptr, &memory);
     if (result2 != VK_SUCCESS)
     {
         throw std::runtime_error(
@@ -101,5 +104,50 @@ create_buffer(const vulkan_scene::Device& p_device, VkDeviceSize p_size)
     }
 
     return {buffer, memory};
+}
+
+// Copies the contents of one buffer onto another one.
+void copy_buffers(const Device& p_device, const VkBuffer p_source,
+                  const VkBuffer p_destination, VkDeviceSize p_size)
+{
+    const auto command_buffer = p_device.allocate_primary_cmd_buffer();
+
+    const VkCommandBufferBeginInfo begin_info{
+        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
+        .pNext = nullptr,
+        .flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT,
+        .pInheritanceInfo = nullptr};
+
+    auto result = vkBeginCommandBuffer(command_buffer, &begin_info);
+    if (result != VK_SUCCESS)
+    {
+        throw std::runtime_error(
+            "Failed to begin recording a command buffer for copying buffers. Vulkan error "s +
+            std::to_string(result) + '.');
+    }
+
+    const VkBufferCopy copy_region{
+        .srcOffset = 0, .dstOffset = 0, .size = p_size};
+
+    vkCmdCopyBuffer(command_buffer, p_source, p_destination, 1, &copy_region);
+
+    result = vkEndCommandBuffer(command_buffer);
+    vulkan_scene_VK_CHECK(result, "end the command buffer for copying buffers");
+
+    const VkSubmitInfo submit_info{.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2,
+                                   .pNext = nullptr,
+                                   .commandBufferCount = 1,
+                                   .pCommandBuffers = &command_buffer};
+
+    result = vkQueueSubmit(p_device.get_graphics_queue(), 1, &submit_info,
+                           VK_NULL_HANDLE);
+    vulkan_scene_VK_CHECK(result,
+                          "submit the command buffer for copying buffers");
+
+    result = vkQueueWaitIdle(p_device.get_graphics_queue());
+    vulkan_scene_VK_CHECK(result,
+                          "wait for the graphics queue to complete operations");
+
+    p_device.free_command_buffer(command_buffer);
 }
 } // namespace
