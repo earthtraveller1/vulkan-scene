adaac47f5e8b85f29cb7cbecdc07faa0783c7688
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-01-23 21:07:07 -0500

    Created the vertex buffer.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 using vulkan_scene::Device;
 using vulkan_scene::Window;
+using vulkan_scene::Vertex;
 
 const uint16_t WIDTH = 1280;
 const uint16_t HEIGHT = 720;
@@ -13,9 +14,18 @@ int main()
     {
         Window window("Vulkan Scene", WIDTH, HEIGHT);
 
-        const Device device("Vulkan Scene", false, window);
+        const Device device("Vulkan Scene", true, window);
         
         const auto swap_chain = device.create_swap_chain(WIDTH, HEIGHT);
+        
+        // TODO: Fill out with actual vertex data.
+        std::vector<Vertex> vertices {
+            { { 0.0f, 0.0f, 0.0f } },
+            { { 0.0f, 0.0f, 0.0f } },
+            { { 0.0f, 0.0f, 0.0f } }
+        };
+        
+        const auto vertex_buffer = device.create_vertex_buffer(vertices);
 
         while (window.is_open())
         {
