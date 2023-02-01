1d17da1b565f39cfc8d09c3f05456456983b3b98
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-01 15:04:03 -0500

    Formatted document.

--- src/buffers.hpp
+++ src/buffers.hpp
@@ -29,11 +29,11 @@ struct Vertex
     {
         return std::array{
   // The position attribute
-            VkVertexInputAttributeDescription{.location = 0,
+            VkVertexInputAttributeDescription{
+                                              .location = 0,
                                               .binding = 0,
                                               .format = VK_FORMAT_R32G32B32_SFLOAT,
-                                              .offset =
-                                                  offsetof(Vertex, position)}
+                                              .offset = offsetof(Vertex, position)}
         };
     }
 };
