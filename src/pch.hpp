f93130ca895ea846477b3296cb6848401b15add9
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-01 14:36:14 -0500

    Added missing headers.

--- src/pch.hpp
+++ src/pch.hpp
@@ -2,12 +2,14 @@
 
 // Basically, the precompiled header.
 
+#include <array>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <vector>
+#include <algorithm>
 
 #ifdef _WIN32
 #define VK_USE_PLATFORM_WIN32_KHR
