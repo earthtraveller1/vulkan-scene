b9bcbb3a41c60c2aaca1f016834fa1c1a8c20c76
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-03 14:21:37 -0500

    Added a bind function.

--- src/graphics-pipeline.hpp
+++ src/graphics-pipeline.hpp
@@ -7,21 +7,28 @@ class Device;
 class GraphicsPipeline
 {
   public:
-    GraphicsPipeline(const Device& device, const SwapChain& swap_chain, std::string_view vertex_path,
+    GraphicsPipeline(const Device& device, const SwapChain& swap_chain,
+                     std::string_view vertex_path,
                      std::string_view fragment_path);
-    
+
     // Disable copying
     GraphicsPipeline(const GraphicsPipeline&) = delete;
     GraphicsPipeline& operator=(const GraphicsPipeline&) = delete;
-    
+
     VkRenderPass get_render_pass_raw_handle() const { return m_render_pass; }
-    
+
+    void cmd_bind(VkCommandBuffer command_buffer) const
+    {
+        vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
+                          m_pipeline);
+    }
+
     ~GraphicsPipeline();
 
   private:
     // Called in the constructor and nowhere else.
     void create_layout();
-    
+
     // Called in the constructor and nowhere else.
     void create_render_pass(const SwapChain& swap_chain);
 
