629f14b32f87974587be67c14eca8d269fcaf15a
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-08 17:33:50 -0500

    Silenced warning + formatted document.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -36,28 +36,32 @@ void Renderer::render()
             .pNext = nullptr,
             .renderPass = m_pipeline.get_render_pass_raw_handle(),
             .framebuffer = m_framebuffers[image_index],
-            .renderArea = {.extent = m_swap_chain.get_extent(),
-                           .offset = {0, 0}},
+            .renderArea = {
+                .offset = {0, 0},
+                .extent = m_swap_chain.get_extent(),
+            },
             .clearValueCount = 1,
             .pClearValues = &clear_value
         };
-        
-        vkCmdBeginRenderPass(m_command_buffer, &render_pass_begin_info, VK_SUBPASS_CONTENTS_INLINE);
-        
+
+        vkCmdBeginRenderPass(m_command_buffer, &render_pass_begin_info,
+                             VK_SUBPASS_CONTENTS_INLINE);
+
         m_pipeline.cmd_bind(m_command_buffer);
         m_vertex_buffer.cmd_bind(m_command_buffer);
-        
+
         vkCmdDraw(m_command_buffer, 3, 1, 0, 0);
-        
+
         vkCmdEndRenderPass(m_command_buffer);
-        
+
         result = vkEndCommandBuffer(m_command_buffer);
         vulkan_scene_VK_CHECK(result, "stop recording the command buffer");
     }
-    
-    const auto wait_stage = static_cast<VkPipelineStageFlags>(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT);
-    
-    const VkSubmitInfo submit_info {
+
+    const auto wait_stage = static_cast<VkPipelineStageFlags>(
+        VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT);
+
+    const VkSubmitInfo submit_info{
         .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
         .pNext = nullptr,
         .waitSemaphoreCount = 1,
@@ -68,11 +72,12 @@ void Renderer::render()
         .signalSemaphoreCount = 1,
         .pSignalSemaphores = &m_render_done_semaphore,
     };
-    
+
     const auto graphics_queue = m_device.get_graphics_queue();
-    
-    auto result = vkQueueSubmit(graphics_queue, 1, &submit_info, VK_NULL_HANDLE);
+
+    auto result =
+        vkQueueSubmit(graphics_queue, 1, &submit_info, VK_NULL_HANDLE);
     vulkan_scene_VK_CHECK(result, "submit the command buffer");
-    
+
     m_swap_chain.present(m_render_done_semaphore, image_index);
 }
