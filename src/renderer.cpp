7350e884c558b61cfc5cfcc2af9461960c8dc346
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-08 17:06:05 -0500

    Finished the draw function.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -54,4 +54,25 @@ void Renderer::render()
         result = vkEndCommandBuffer(m_command_buffer);
         vulkan_scene_VK_CHECK(result, "stop recording the command buffer");
     }
+    
+    const auto wait_stage = static_cast<VkPipelineStageFlags>(VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT);
+    
+    const VkSubmitInfo submit_info {
+        .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
+        .pNext = nullptr,
+        .waitSemaphoreCount = 1,
+        .pWaitSemaphores = &m_image_available_semaphore,
+        .pWaitDstStageMask = &wait_stage,
+        .commandBufferCount = 1,
+        .pCommandBuffers = &m_command_buffer,
+        .signalSemaphoreCount = 1,
+        .pSignalSemaphores = &m_render_done_semaphore,
+    };
+    
+    const auto graphics_queue = m_device.get_graphics_queue();
+    
+    auto result = vkQueueSubmit(graphics_queue, 1, &submit_info, VK_NULL_HANDLE);
+    vulkan_scene_VK_CHECK(result, "submit the command buffer");
+    
+    m_swap_chain.present(m_render_done_semaphore, image_index);
 }
