a3b5dbc3fdb67c0654b41ad1da53d0e607b2c530
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-08 19:18:10 -0500

    Formattedd the document.

--- src/swap-chain.hpp
+++ src/swap-chain.hpp
@@ -22,9 +22,9 @@ class SwapChain
     VkFormat get_format() const { return m_format; }
 
     const VkExtent2D& get_extent() const { return m_extent; }
-    
-    // Acquires an image from the swap chain. This function is specifically suited
-    // for the Renderer class.
+
+    // Acquires an image from the swap chain. This function is specifically
+    // suited for the Renderer class.
     inline uint32_t acquire_next_image(VkSemaphore semaphore) const
     {
         uint32_t image_index;
@@ -33,22 +33,22 @@ class SwapChain
                               VK_NULL_HANDLE, &image_index);
         return image_index;
     }
-    
+
     // Presents an image to the screen.
     inline void present(VkSemaphore wait_semaphore, uint32_t image_index) const
     {
-        const VkPresentInfoKHR present_info {
-          .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-          .pNext = nullptr,
-          .waitSemaphoreCount = 1,
-          .pWaitSemaphores = &wait_semaphore,
-          .swapchainCount = 1,
-          .pSwapchains = &m_swap_chain,
-          .pImageIndices = &image_index,
-          .pResults = nullptr
-        };
-        
-        const auto result = vkQueuePresentKHR(m_device.get_present_queue(), &present_info);
+        const VkPresentInfoKHR present_info{
+            .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
+            .pNext = nullptr,
+            .waitSemaphoreCount = 1,
+            .pWaitSemaphores = &wait_semaphore,
+            .swapchainCount = 1,
+            .pSwapchains = &m_swap_chain,
+            .pImageIndices = &image_index,
+            .pResults = nullptr};
+
+        const auto result =
+            vkQueuePresentKHR(m_device.get_present_queue(), &present_info);
         vulkan_scene_VK_CHECK(result, "present a swap chain");
     }
 
