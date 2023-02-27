5d0fbc2e37fbad4a14e5299eef7023273d14845d
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-26 19:11:54 -0500

    Implemented matrices and matrix multiplication.

--- src/math.hpp
+++ src/math.hpp
@@ -153,4 +153,59 @@ using Vector2f = Vector2<float>;
 using Vector3f = Vector3<float>;
 using Vector4f = Vector4<float>;
 
+// A 4 by 4 matrix.
+template<Scalar T> struct Matrix4 
+{
+    T rows[4][4];
+    
+    // Constructs an identity matrix with the specified constant.
+    Matrix(T x = 1)
+    {
+        rows[0][0] = x;
+        rows[0][1] = 0;
+        rows[0][2] = 0;
+        rows[0][3] = 0;
+        
+        rows[1][0] = 0;
+        rows[1][1] = x;
+        rows[1][2] = 0;
+        rows[1][3] = 0;
+        
+        rows[2][0] = 0;
+        rows[2][1] = 0;
+        rows[2][2] = x;
+        rows[2][3] = 0;
+        
+        rows[3][0] = 0;
+        rows[3][1] = 0;
+        rows[3][2] = 0;
+        rows[3][3] = x;
+    }
+    
+    // Utility function for reducing the amount of code required in the matrix multiplication 
+    private: inline static T dot_rows_and_columns(const Matrix4<T>& a, const Matrix4<T>& b, uint8_t row, uint8_t column)
+    {
+        return a.rows[row][0] * b.rows[0][column] + a.rows[row][1] * b.rows[1][column] + a.rows[row][2] * b.rows[2][column] * a.rows[row][3] * b.rows[3][column];
+    }
+
+    // This function multiplies two matrices. Hopefully, in the future, I won't have
+    // to tamper with this function, as it is very annoying to work with.
+    public: Matrix4<T> operator*(const Matrix4& b)
+    {
+        Matrix4<T> result;
+        
+        // Most compilers should unroll this loop. Well, they should in release
+        // mode, that is.
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                dot_rows_and_columns(*this, b, i, j);
+            }
+        }
+        
+        return result;
+    }
+};
+
 } // namespace vulkan_scene
