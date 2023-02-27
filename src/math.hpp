ec869c287b49d36bf2c1842f0d4ce6a8d8dbfd10
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-02-26 19:31:31 -0500

    Formatted document + added alias for float matrices.

--- src/math.hpp
+++ src/math.hpp
@@ -29,52 +29,52 @@ concept Scalar = requires(T a, T b) {
 template <Scalar T, std::size_t Size> struct Vector
 {
     T v[Size];
-    
+
     Vector<T, Size> operator+(const Vector<T, Size>& b)
     {
         Vector result;
-        
+
         for (size_t i = 0; i < Size; i++)
         {
             result.v[i] = v[i] + b.v[i];
         }
-        
+
         return result;
     }
-    
+
     Vector<T, Size> operator-(const Vector<T, Size>& b)
     {
         Vector result;
-        
+
         for (size_t i = 0; i < Size; i++)
         {
             result.v[i] = v[i] - b.v[i];
         }
-        
+
         return result;
     }
-    
+
     Vector<T, Size> operator*(T b)
     {
         Vector result;
-        
+
         for (size_t i = 0; i < size; i++)
         {
             result.v[i] = v[i] * b;
         }
-        
+
         return result;
     }
-    
+
     T dot(const Vector<T, Size>& b)
     {
         T result;
-        
+
         for (size_t i = 0; i < Size; i++)
         {
             result += v[i] * b.v[i];
         }
-        
+
         return result;
     }
 };
@@ -94,7 +94,7 @@ template <Scalar T, std::size_t Size> struct Vector
 template <typename T> struct Vector<T, 2>
 {
     T v[2];
-    
+
     inline Vector(T x = 0, T y = 0)
     {
         v[0] = x;
@@ -109,7 +109,7 @@ template <typename T> struct Vector<T, 2>
 template <typename T> struct Vector<T, 3>
 {
     T v[3];
-    
+
     inline Vector(T x = 0, T y = 0, T z = 0)
     {
         v[0] = x;
@@ -126,7 +126,7 @@ template <typename T> struct Vector<T, 3>
 template <typename T> struct Vector<T, 4>
 {
     T v[4];
-    
+
     inline Vector(T x = 0, T y = 0, T z = 0, T w = 0)
     {
         v[0] = x;
@@ -154,10 +154,10 @@ using Vector3f = Vector3<float>;
 using Vector4f = Vector4<float>;
 
 // A 4 by 4 matrix.
-template<Scalar T> struct Matrix4 
+template <Scalar T> struct Matrix4
 {
     T rows[4][4];
-    
+
     // Constructs an identity matrix with the specified constant.
     Matrix(T x = 1)
     {
@@ -165,35 +165,43 @@ template<Scalar T> struct Matrix4
         rows[0][1] = 0;
         rows[0][2] = 0;
         rows[0][3] = 0;
-        
+
         rows[1][0] = 0;
         rows[1][1] = x;
         rows[1][2] = 0;
         rows[1][3] = 0;
-        
+
         rows[2][0] = 0;
         rows[2][1] = 0;
         rows[2][2] = x;
         rows[2][3] = 0;
-        
+
         rows[3][0] = 0;
         rows[3][1] = 0;
         rows[3][2] = 0;
         rows[3][3] = x;
     }
-    
-    // Utility function for reducing the amount of code required in the matrix multiplication 
-    private: inline static T dot_rows_and_columns(const Matrix4<T>& a, const Matrix4<T>& b, uint8_t row, uint8_t column)
+
+    // Utility function for reducing the amount of code required in the matrix
+    // multiplication
+  private:
+    inline static T dot_rows_and_columns(const Matrix4<T>& a,
+                                         const Matrix4<T>& b, uint8_t row,
+                                         uint8_t column)
     {
-        return a.rows[row][0] * b.rows[0][column] + a.rows[row][1] * b.rows[1][column] + a.rows[row][2] * b.rows[2][column] * a.rows[row][3] * b.rows[3][column];
+        return a.rows[row][0] * b.rows[0][column] +
+               a.rows[row][1] * b.rows[1][column] +
+               a.rows[row][2] * b.rows[2][column] * a.rows[row][3] *
+                   b.rows[3][column];
     }
 
-    // This function multiplies two matrices. Hopefully, in the future, I won't have
-    // to tamper with this function, as it is very annoying to work with.
-    public: Matrix4<T> operator*(const Matrix4& b)
+    // This function multiplies two matrices. Hopefully, in the future, I won't
+    // have to tamper with this function, as it is very annoying to work with.
+  public:
+    Matrix4<T> operator*(const Matrix4& b)
     {
         Matrix4<T> result;
-        
+
         // Most compilers should unroll this loop. Well, they should in release
         // mode, that is.
         for (int i = 0; i < 4; i++)
@@ -203,9 +211,12 @@ template<Scalar T> struct Matrix4
                 dot_rows_and_columns(*this, b, i, j);
             }
         }
-        
+
         return result;
     }
 };
+
+// An alias for Matrices that uses floats.
+using Matrix4f = Matrix4<float>;
 
 } // namespace vulkan_scene
