5daa0bebcef484c5c5a367b7a0c57469ff0ef55a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-04-17 17:27:10 -0400

    Created the Window surface object.

--- src/device.c
+++ src/device.c
@@ -1,13 +1,19 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-#include <vulkan/vulkan.h>
-#include <vulkan/vulkan_core.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include "device.h"
+#include "window.h"
 
 /* The internal objects */ 
 static VkInstance instance;
+
+/* The Window surface. This will be needed when we present
+ * shit to the screen. */
+static VkSurfaceKHR window_surface;
 
 /* Creates the instance specifically. */ 
 static bool create_instance()
@@ -20,6 +26,9 @@ static bool create_instance()
     app_info.pEngineName = NULL;
     app_info.engineVersion = 0;
     app_info.apiVersion = VK_API_VERSION_1_2;
+
+    uint32_t extension_count;
+    const char* const* const glfw_extensions = glfwGetRequiredInstanceExtensions(&extension_count);
     
     VkInstanceCreateInfo create_info;
     create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -28,8 +37,8 @@ static bool create_instance()
     create_info.pApplicationInfo = &app_info;
     create_info.enabledLayerCount = 0;
     create_info.ppEnabledLayerNames = NULL;
-    create_info.enabledExtensionCount = 0;
-    create_info.ppEnabledExtensionNames = NULL;
+    create_info.enabledExtensionCount = extension_count;
+    create_info.ppEnabledExtensionNames = glfw_extensions;
 
     VkResult result = vkCreateInstance(&create_info, NULL, &instance);
     if (result != VK_SUCCESS)
@@ -44,9 +53,10 @@ static bool create_instance()
 bool create_device()
 {
     if (!create_instance())
-    {
         return false;
-    }
+    
+    if (!get_window_surface(instance, &window_surface))
+        return false;
 
     return true;
 }
@@ -54,4 +64,5 @@ bool create_device()
 void destroy_device()
 {
     vkDestroyInstance(instance, NULL);
+    vkDestroySurfaceKHR(instance, window_surface, NULL);
 }
