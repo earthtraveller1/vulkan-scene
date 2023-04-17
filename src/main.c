5aa6ec7b7b675d37ef1eed9047a0d7b7a74e00e1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-04-17 17:05:44 -0400

    Created a window.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 #include "device.h"
+#include "window.h"
 
 int main()
 {
     create_device();
 
+    create_window();
+
+    while (is_window_open())
+    {
+        update_window();
+    }
+
+    destroy_window();
     destroy_device();
 }
