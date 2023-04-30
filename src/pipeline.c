bb75dcb38738d3af395ca7e31df8877edf86c972
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-29 20:35:24 -0400

    Wrote the function for creating a shader module.

--- src/pipeline.c
+++ src/pipeline.c
@@ -1,11 +1,48 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
 
 #include "graphics.h"
 
 /* Contains all the implementation details relating to the graphics pipeline abstraction */
+
+/* Creates a shader module by loading the SPIR-V from disk */
+static bool create_shader_module_from_file(VkDevice p_device, const char* p_path, VkShaderModule* p_module)
+{
+    FILE* file = fopen(p_path, "r");
+
+    /* Obtain the size of the file by seeking to the end and obtaining the position. */
+    fseek(file, 0, SEEK_END);
+    size_t file_size = ftell(file);
+    fseek(file, 0, SEEK_SET); /* Remember to go back to the beginning */
+
+    char* file_contents = malloc(sizeof(char) * file_size);
+    fread(file_contents, sizeof(char), file_size, file);
+
+    fclose(file);
+
+    VkShaderModuleCreateInfo create_info;
+    create_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    create_info.pNext = NULL;
+    create_info.flags = 0;
+    create_info.codeSize = file_size;
+    create_info.pCode = (uint32_t*)file_contents;
+
+    VkResult result = vkCreateShaderModule(p_device, &create_info, NULL, p_module);
+
+    if (result != VK_SUCCESS)
+    {
+        fprintf(stderr, "\033[91m[ERROR]: Failed to create shader module %s. Vulkan error %d\033[0m\n", p_path, result);
+        return false;
+    }
+
+    free(file_contents);
+
+    return true;
+}
 
 bool create_render_pass(VkDevice p_device, VkFormat p_swap_chain_format, VkRenderPass* p_render_pass)
 {
@@ -48,7 +85,7 @@ bool create_render_pass(VkDevice p_device, VkFormat p_swap_chain_format, VkRende
     create_info.pDependencies = NULL;
 
     VkResult result = vkCreateRenderPass(p_device, &create_info, NULL, p_render_pass);
-    
+
     if (result != VK_SUCCESS)
     {
         fprintf(stderr, "\033[91m[ERROR]: Failed to create a render pass. Vulkan error %d.\033[0m\n", result);
