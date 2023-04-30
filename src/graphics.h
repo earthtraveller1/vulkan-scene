24b0a1d8bf6f29790232b14bd96bf28533d0e5c3
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-29 20:21:48 -0400

    Wrote a function for creating the render pass.

---
+++ src/graphics.h
@@ -0,0 +1,26 @@
+#ifndef INCLUDED_graphics_h
+#define INCLUDED_graphics_h
+
+#include <stdbool.h>
+
+#include <vulkan/vulkan.h>
+
+/* This file contains all of the general purposed graphics-related components, including the graphics pipeline, the buffers,
+ * the textures, etc. */
+
+/* A single vertex. */
+struct vertex
+{
+    float position[3];
+};
+
+struct graphics_pipeline
+{
+    VkPipelineLayout layout;
+    VkPipeline pipeline;
+};
+
+/* Creates a render pass. The boolean value that is returned indicates whether the operation succeeded or not. */
+bool create_render_pass(VkDevice p_device, VkFormat p_swap_chain_format, VkRenderPass* p_render_pass);
+
+#endif
---
+++ src/pipeline.c
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#include <vulkan/vulkan.h>
+
+#include "graphics.h"
+
+/* Contains all the implementation details relating to the graphics pipeline abstraction */
+
+bool create_render_pass(VkDevice p_device, VkFormat p_swap_chain_format, VkRenderPass* p_render_pass)
+{
+    VkAttachmentDescription color_attachment;
+    color_attachment.flags = 0;
+    color_attachment.format = p_swap_chain_format;
+    color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+    color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+    color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+    color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    VkAttachmentReference color_attachment_ref;
+    color_attachment_ref.attachment = 0;
+    color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass;
+    subpass.flags = 0;
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+    subpass.inputAttachmentCount = 0;
+    subpass.pInputAttachments = NULL;
+    subpass.colorAttachmentCount = 1;
+    subpass.pColorAttachments = &color_attachment_ref;
+    subpass.pResolveAttachments = NULL;
+    subpass.pDepthStencilAttachment = NULL;
+    subpass.preserveAttachmentCount = 0;
+    subpass.pPreserveAttachments = NULL;
+
+    VkRenderPassCreateInfo create_info;
+    create_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    create_info.pNext = NULL;
+    create_info.flags = 0;
+    create_info.attachmentCount = 1;
+    create_info.pAttachments = &color_attachment;
+    create_info.subpassCount = 1;
+    create_info.pSubpasses = &subpass;
+    create_info.dependencyCount = 0;
+    create_info.pDependencies = NULL;
+
+    VkResult result = vkCreateRenderPass(p_device, &create_info, NULL, p_render_pass);
+    
+    if (result != VK_SUCCESS)
+    {
+        fprintf(stderr, "\033[91m[ERROR]: Failed to create a render pass. Vulkan error %d.\033[0m\n", result);
+        return false;
+    }
+
+    return true;
+}
