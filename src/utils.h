fe90844adaa0ff21a842bcd7a66cedeb4b10b7b6
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-05-22 13:37:00 -0400

    Fixed a small compilation error.

--- src/utils.h
+++ src/utils.h
@@ -6,7 +6,7 @@
 #define HANDLE_VK_ERROR(result, message, fail_action)                                                                                      \
     if (result != VK_SUCCESS)                                                                                                              \
     {                                                                                                                                      \
-        fprintf("\033[[ERROR]: Failed to " message ". Vulkan error %d.\033[0m\n", result);                                                 \
+        fprintf(stderr, "\033[[ERROR]: Failed to " message ". Vulkan error %d.\033[0m\n", result);                                         \
         fail_action;                                                                                                                       \
     }
 
