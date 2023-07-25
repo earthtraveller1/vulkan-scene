4d58bc08fead2f8a71f983f40632e5749fada278
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-25 14:45:26 -0400

    Initial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# vulkan-scene
-
-A 3D Vulkan scene with lighting and 3D objects.
--- shaders/push-constants.glsl
+++
@@ -1,6 +0,0 @@
-layout (push_constant) uniform constant
-{
-    float position_shift;
-    float color_shift;
-} push_constants;
-
