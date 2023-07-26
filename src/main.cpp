02670aafff1f31359dbec3cc63b5d256603f6d54
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-26 14:21:48 -0400

    Implemented defer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+template<typename F>
+struct defer
+{
+    defer(F f): m_f(f) {}
+    ~defer() { m_f(); }
+    F m_f;
+};
+
+#define defer(name, statement) const auto name##_defer = defer {[](){ statement; }}; (void)name##_defer
+
 auto main() -> int
 {
     std::cout << "Hello, World!\n";
