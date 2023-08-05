98878f5b8b0bd4b5e44b14066a9fb883b9753eb9
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-05 11:16:17 -0400

    Added some more headers to the precompiled header.

--- src/pch.hpp
+++ src/pch.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <cstdint>
+
 #include <vector>
 #include <array>
 #include <iostream>
 #include <memory>
 #include <optional>
+#include <limits>
+#include <algorithm>
 
 #include <kirho/kirho.hpp>
