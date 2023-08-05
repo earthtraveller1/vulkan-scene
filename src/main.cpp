74161d73e5a44dc53a026a92e483d2a8671c6d46
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-05 11:31:06 -0400

    Created the swap chain.

--- src/main.cpp
+++ src/main.cpp
@@ -405,6 +405,109 @@ auto create_logical_device(VkPhysicalDevice p_physical_device, uint32_t p_graphi
     return result_t::success(device);
 }
 
+auto create_swapchain(
+        VkDevice p_device, 
+        VkPhysicalDevice p_physical_device, 
+        uint32_t p_graphics_family, 
+        uint32_t p_present_family, 
+        GLFWwindow* p_window, 
+        VkSurfaceKHR p_surface
+) -> result<VkSwapchainKHR, VkResult>
+{
+    VkSurfaceCapabilitiesKHR surface_capabilities;
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(p_physical_device, p_surface, &surface_capabilities);
+
+    uint32_t surface_format_count;
+    vkGetPhysicalDeviceSurfaceFormatsKHR(p_physical_device, p_surface, &surface_format_count, nullptr);
+
+    std::vector<VkSurfaceFormatKHR> surface_formats(surface_format_count);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(p_physical_device, p_surface, &surface_format_count, surface_formats.data());
+
+    uint32_t present_mode_count;
+    vkGetPhysicalDeviceSurfacePresentModesKHR(p_physical_device, p_surface, &present_mode_count, nullptr);
+
+    std::vector<VkPresentModeKHR> present_modes(present_mode_count);
+    vkGetPhysicalDeviceSurfacePresentModesKHR(p_physical_device, p_surface, &present_mode_count, present_modes.data());
+
+    VkSurfaceFormatKHR surface_format = surface_formats.front();
+
+    for (const auto& format : surface_formats)
+    {
+        if (format.format == VK_FORMAT_B8G8R8A8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+        {
+            surface_format = format;
+            break;
+        }
+    }
+
+    VkPresentModeKHR present_mode = VK_PRESENT_MODE_FIFO_KHR;
+
+    for (auto mode : present_modes)
+    {
+        if (mode == VK_PRESENT_MODE_MAILBOX_KHR)
+        {
+            present_mode = mode;
+            break;
+        }
+    }
+
+    int framebuffer_width, framebuffer_height;
+    glfwGetFramebufferSize(p_window, &framebuffer_width, &framebuffer_height);
+
+    const VkExtent2D swap_extent = 
+        surface_capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max() 
+            ? surface_capabilities.currentExtent 
+            : VkExtent2D {
+                std::clamp(static_cast<uint32_t>(framebuffer_width), surface_capabilities.minImageExtent.width, surface_capabilities.maxImageExtent.width),
+                std::clamp(static_cast<uint32_t>(framebuffer_height), surface_capabilities.minImageExtent.height, surface_capabilities.maxImageExtent.height),
+            };
+
+    uint32_t image_count = surface_capabilities.minImageCount + 1;
+    const auto has_max_image_count = surface_capabilities.maxImageCount > 0;
+    if (has_max_image_count && image_count > surface_capabilities.maxImageCount)
+    {
+        image_count = surface_capabilities.maxImageCount;
+    }
+
+    const auto queue_families_same = p_graphics_family == p_present_family;
+
+    std::array<uint32_t, 2> queue_families{p_graphics_family, p_present_family};
+
+    const VkSwapchainCreateInfoKHR swapchain_info{
+        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .pNext = nullptr,
+        .flags = 0,
+        .surface = p_surface,
+        .minImageCount = image_count,
+        .imageFormat = surface_format.format,
+        .imageColorSpace = surface_format.colorSpace,
+        .imageExtent = swap_extent,
+        .imageArrayLayers = 1,
+        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        .imageSharingMode = queue_families_same ? VK_SHARING_MODE_EXCLUSIVE : VK_SHARING_MODE_CONCURRENT,
+        .queueFamilyIndexCount = queue_families_same ? 0 : static_cast<uint32_t>(queue_families.size()),
+        .pQueueFamilyIndices = queue_families_same ? nullptr : queue_families.data(),
+        .preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        .presentMode = present_mode,
+        .clipped = VK_TRUE,
+        .oldSwapchain = VK_NULL_HANDLE,
+    };
+
+    VkSwapchainKHR swapchain;
+
+    using result_t = result<VkSwapchainKHR, VkResult>;
+
+    const auto result = vkCreateSwapchainKHR(p_device, &swapchain_info, nullptr, &swapchain);
+    if (result != VK_SUCCESS)
+    {
+        print_error("Failed to create the swapchain. Vulkan error ", result);
+        return result_t::error(result);
+    }
+
+    return result_t::success(swapchain);
+}
+
 auto destroy_debug_messenger(VkInstance p_instance, VkDebugUtilsMessengerEXT p_messenger)
 {
     const auto function = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(vkGetInstanceProcAddr(p_instance, "vkDestroyDebugUtilsMessengerEXT"));
@@ -438,6 +541,9 @@ auto main() noexcept -> int
 
     const auto logical_device = create_logical_device(physical_device, graphics_queue_family, present_queue_family).unwrap();
     defer(logical_device, vkDestroyDevice(logical_device, nullptr));
+
+    const auto swapchain = create_swapchain(logical_device, physical_device, graphics_queue_family, present_queue_family, window, surface).unwrap();
+    defer(swapchain, vkDestroySwapchainKHR(logical_device, swapchain, nullptr));
 
     while (!glfwWindowShouldClose(window))
     {
