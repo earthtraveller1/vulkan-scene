f79fea2ead14e6c86937d31b451a210599aa17d9
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-10 12:00:47 -0400

    Started on the create_buffer function. Created the staging buffer.

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,8 +1,35 @@
+#include <cstring>
+
 #include <fstream>
 
 #include "common.hpp"
 
 #include "graphics.hpp"
+
+namespace {
+
+auto find_buffer_memory_type(VkPhysicalDevice p_device, uint32_t p_type_filter,
+                             VkMemoryPropertyFlags p_memory_properties)
+    -> kirho::result_t<uint32_t, kirho::empty> {
+  using result_t = kirho::result_t<uint32_t, kirho::empty>;
+
+  VkPhysicalDeviceMemoryProperties memory_properties;
+  vkGetPhysicalDeviceMemoryProperties(p_device, &memory_properties);
+
+  for (uint32_t i = 0; i < memory_properties.memoryTypeCount; i++) {
+    const auto follows_filter = (p_type_filter & (1 << i)) != 0;
+    const auto has_properties =
+        (memory_properties.memoryTypes[i].propertyFlags &
+         p_memory_properties) == p_memory_properties;
+
+    if (follows_filter && has_properties) {
+      return result_t::success(i);
+    }
+  }
+
+  return result_t::error(kirho::empty{});
+}
+} // namespace
 
 namespace vulkan_scene {
 
@@ -228,6 +255,72 @@ auto create_graphics_pipeline(VkDevice p_device, VkRenderPass p_render_pass,
   }
 
   return result_tt::success(pipeline);
+}
+
+auto create_buffer(VkPhysicalDevice p_physical_device, VkDevice p_device,
+                   buffer_type_t p_type, const void *p_data,
+                   VkDeviceSize p_data_size) noexcept
+    -> kirho::result_t<buffer_t, VkResult> {
+  using result_t = kirho::result_t<buffer_t, VkResult>;
+
+  const VkBufferCreateInfo staging_buffer_info{
+      .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+      .pNext = nullptr,
+      .flags = 0,
+      .size = p_data_size,
+      .usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+      .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+      .queueFamilyIndexCount = 0,
+      .pQueueFamilyIndices = nullptr,
+  };
+
+  VkBuffer staging_buffer;
+  auto result =
+      vkCreateBuffer(p_device, &staging_buffer_info, nullptr, &staging_buffer);
+  if (result != VK_SUCCESS) {
+    print_error("Failed to create a Vulkan buffer. Vulkan error ", result, '.');
+    return result_t::error(result);
+  }
+
+  VkMemoryRequirements memory_requirements;
+  vkGetBufferMemoryRequirements(p_device, staging_buffer, &memory_requirements);
+
+  const auto memory_type =
+      find_buffer_memory_type(p_physical_device,
+                              memory_requirements.memoryTypeBits,
+                              VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT |
+                                  VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)
+          .unwrap();
+
+  const VkMemoryAllocateInfo memory_alloc_info{
+      .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+      .pNext = nullptr,
+      .allocationSize = memory_requirements.size,
+      .memoryTypeIndex = memory_type,
+  };
+
+  VkDeviceMemory staging_buffer_memory;
+  result = vkAllocateMemory(p_device, &memory_alloc_info, nullptr,
+                            &staging_buffer_memory);
+  if (result != VK_SUCCESS) {
+    print_error("Failed to allocate memory for a staging buffer. Vulkan error ",
+                result, '.');
+    return result_t::error(result);
+  }
+
+  vkBindBufferMemory(p_device, staging_buffer, staging_buffer_memory, 0);
+
+  void *staging_buffer_pointer = nullptr;
+  vkMapMemory(p_device, staging_buffer_memory, 0, p_data_size, 0,
+              &staging_buffer_pointer);
+  std::memcpy(staging_buffer_pointer, p_data, p_data_size);
+  vkUnmapMemory(p_device, staging_buffer_memory);
+
+  return result_t::success(buffer_t{
+      .buffer = staging_buffer,
+      .memory = staging_buffer_memory,
+      .type = p_type,
+  });
 }
 
 } // namespace vulkan_scene
