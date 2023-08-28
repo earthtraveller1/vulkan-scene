65f27fbbfd26ec337948ddd765c03dfc79029a47
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-08-28 11:45:58 -0400

    Created the sampler and moved the descriptor writes. Also moved the descriptor writes to after we created the images.

--- src/main.cpp
+++ src/main.cpp
@@ -324,29 +324,6 @@ auto main(int argc, char** argv) noexcept -> int
         create_descriptor_set(device, descriptor_pool, descriptor_set_layout)
             .unwrap();
 
-    {
-        const VkDescriptorBufferInfo uniform_buffer_info{
-            .buffer = uniform_buffer.buffer,
-            .offset = 0,
-            .range = sizeof(uniform_buffer_data),
-        };
-
-        const VkWriteDescriptorSet set_write{
-            .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-            .pNext = nullptr,
-            .dstSet = descriptor_set,
-            .dstBinding = 0,
-            .dstArrayElement = 0,
-            .descriptorCount = 1,
-            .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
-            .pImageInfo = nullptr,
-            .pBufferInfo = &uniform_buffer_info,
-            .pTexelBufferView = nullptr,
-        };
-
-        vkUpdateDescriptorSets(device, 1, &set_write, 0, nullptr);
-    }
-
     const auto indices = std::array<uint16_t, 6>{0, 1, 2, 0, 2, 3};
 
     const auto vertices = std::array<vulkan_scene::vertex_t, 4>{
@@ -378,6 +355,34 @@ auto main(int argc, char** argv) noexcept -> int
             "textures/can-pooper.png"
         )
             .unwrap();
+
+    const auto sampler = vulkan_scene::create_sampler(
+                             device, VK_FILTER_LINEAR, VK_FILTER_LINEAR, false
+    )
+                             .unwrap();
+
+    {
+        const VkDescriptorBufferInfo uniform_buffer_info{
+            .buffer = uniform_buffer.buffer,
+            .offset = 0,
+            .range = sizeof(uniform_buffer_data),
+        };
+
+        const VkWriteDescriptorSet set_write{
+            .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+            .pNext = nullptr,
+            .dstSet = descriptor_set,
+            .dstBinding = 0,
+            .dstArrayElement = 0,
+            .descriptorCount = 1,
+            .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+            .pImageInfo = nullptr,
+            .pBufferInfo = &uniform_buffer_info,
+            .pTexelBufferView = nullptr,
+        };
+
+        vkUpdateDescriptorSets(device, 1, &set_write, 0, nullptr);
+    }
 
     using vulkan_scene::print_error;
 
