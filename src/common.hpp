38f1c8d7164a483c37aeb5b5a35306a725d35ad0
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-13 17:19:17 -0400

    Implemented the << operator for printing containers.

--- src/common.hpp
+++ src/common.hpp
@@ -5,11 +5,34 @@
 namespace vulkan_scene
 {
 
+template <typename T>
+concept iterable_c = requires(T a) {
+    {
+        a.begin()
+    };
+    {
+        a.end()
+    };
+};
+
 template <kirho::printable_t... T>
 auto print_error(T... args) noexcept
 {
     std::cerr << "\033[91m[ERROR]: ";
     (std::cerr << ... << args) << "\033[0m\n";
+}
+
+template <iterable_c T>
+auto operator<<(std::ostream& p_stream, T p_container) -> std::ostream&
+{
+    p_stream << "[ ";
+
+    for (const auto& value : p_container)
+    {
+        p_stream << value << ", ";
+    }
+
+    p_stream << " ]";
 }
 
 } // namespace vulkan_scene
